5e61cbf932e9d734030f18f926f893f893572f30
Leonardo Souza <leonado_b22_@hotmail.com>
2022-01-21 00:31:40 -0300

    adjustment in router of index

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+app.get("/", (req, res) => {
+    return res.json({ message: "Module immobile running" });
+  });
 require('./controllers/imovelController')(app);
 
 
