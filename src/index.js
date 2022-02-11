2d3b9722c0cc36497efdd362197b284cf116a410
Leonardo Souza <leonado_b22_@hotmail.com>
2022-02-10 21:37:45 -0300

    Ajuste de deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const connectDB = require('./database/configDB')
 const bodyParser = require('body-parser');
 
 const app = express();
-
+const PORT = process.env.PORT || 3001 
 connectDB();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
@@ -11,4 +11,4 @@ app.use(bodyParser.urlencoded({extended:false}));
 require('./controllers/imovelController')(app);
 
 
-app.listen(3001, () => console.log("\n||======> MODULE IMMOBILE STARTED <======||"));
+app.listen(PORT, () => console.log("\n||======> MODULE IMMOBILE STARTED <======||"));
